bf884cfeb99c2a86b3e8eaa216e3f03165c1196d
Guzeur <sr.guzmman@gmail.com>
2026-02-23 18:29:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -81,6 +81,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(Servidor Pro corriendo en puerto ${PORT});
+  console.log("Servidor Pro corriendo en puerto" + PORT);
 });
+
 
